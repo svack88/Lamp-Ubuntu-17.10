bd9ef9f4209c48d469bb73f74f9852d970887e46
svack88 <40365402+svack88@users.noreply.github.com>
2018-06-18 17:49:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-apt-get update && apt-get upgrade && apt-get install apache2 php7.1 libapache2-mod-php7.1 php7.1-mysql php-common php7.1-cli php7.1-common php7.1-json php7.1-opcache php7.1-readline mysql-server-5.7 phpmyadmin php-mbstring php-gettext &&  a2enmod php7.1 && systemctl restart apache2
+apt-get update && apt-get upgrade && apt-get install apache2 php7.2 libapache2-mod-php7.2 php7.2-mysql php-common php7.2-cli php7.2-common php7.2-json php7.2-opcache php7.2-readline mysql-server phpmyadmin php-mbstring php-gettext &&  a2enmod php7.2 && systemctl restart apache2
